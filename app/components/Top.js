1941fe1d29df0a91e464b3520083f7a772289484
Daniel <dahnguyen@ucdavis.edu>
2019-09-04 10:55:18 -0700

    changed function in Top component to getinfo into state array

--- app/components/Top.js
+++ app/components/Top.js
@@ -1,44 +1,32 @@
 import React from "react";
 import { getInfo } from '../utils/api'
 
-function idGrid (ids) {
-  return (
-    <ul>
-     {ids.map((id) => (
-      //  getInfo(id)
-      //   .then(data => {
-      //     const {
-      //       by,
-      //       descendents,
-      //       kids,
-      //       score,
-      //       time,
-      //       title,
-      //       type,
-      //       url 
-      //     } = data
-      //    return (
-      //      <li>
-      //        <h1>{title}</h1>
-      //      </li>
-      //    )
-      //   })
-      <li>{id}</li>
-    ))}
-    </ul>
-  )
-}
 
 export default class Top extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      top: [],
+    }
+    this.getTopInfo = this.getTopInfo.bind(this);
   }
 
+  getTopInfo (ids) {
+    ids.map((id) => (
+      getInfo(id)
+        .then(data => {
+          this.setState({
+            top: data
+          })
+        })
+    ))
+  }
 
   render() {
     return (
       <React.Fragment>
-        {idGrid(this.props.ids)}
+        {this.getTopInfo(this.props.ids)}
         <h1>test</h1>
       </React.Fragment>
     )
